b6a27bc909bad9c0bcded8144173a14f5c20c61f
TanmayMene <tanmaymene2004@gmail.com>
2023-11-11 20:57:09 +0530

    changed button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({styles}) => {
   return (
     <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[20px] ${styles}`}>
-      Sign up Now
+      Sign up now
     </button>
   )
 }
